a6599c1f8be1c9a72bf8e1b4193dcf9ffe8c080d
vahidkk <kohandel.vahid@gmail.com>
2022-02-21 12:17:08 +0330

    error handlers added

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 //connect  db
 const connectDB = require("./db/connect");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
