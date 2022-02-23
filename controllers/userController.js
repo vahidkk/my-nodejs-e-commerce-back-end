f8b1fa1b61bc4b96aa344c190e63759902563a6b
vahidkk <kohandel.vahid@gmail.com>
2022-02-23 22:13:10 +0330

    authenticatin added

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,15 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 
 const getAllUsers = async (req, res) => {
-  res.send("ok");
+  const users = await User.find({ role: "user" }).select("-password");
+  res.status(StatusCodes.OK).json({ users });
 };
-
 const getSingleUser = async (req, res) => {
-  res.send("ok");
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 const updateUser = async (req, res) => {
   res.send("ok");
