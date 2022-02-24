70447e079d660abff4291f78186b9c18be3c3f4b
vahidkk <kohandel.vahid@gmail.com>
2022-02-24 20:25:53 +0330

    product model and router and cotroller added

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const connectDB = require("./db/connect");
 // routers:
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -24,6 +25,7 @@ app.get("/", (req, res) => {
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
