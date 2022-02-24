31b088803010d695a3acce4810112c1033f22604
vahidkk <kohandel.vahid@gmail.com>
2022-02-24 12:18:29 +0330

    update password and update user added

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,14 +13,41 @@ const getSingleUser = async (req, res) => {
   }
   res.status(StatusCodes.OK).json({ user });
 };
-const updateUser = async (req, res) => {
-  res.send("ok");
-};
 const showCurrentUser = async (req, res) => {
-  res.send("ok");
+  res.status(StatusCodes.OK).json({ user: req.user });
 };
+const updateUser = async (req, res) => {
+  const { email, name } = req.body;
+  if (!email || !name) {
+    throw new CustomError.BadRequestError("Please provide all values");
+  }
+  const user = await User.findOne({ _id: req.user.userId });
+
+  user.email = email;
+  user.name = name;
+
+  await user.save();
+
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
+};
+
 const updateUserPassword = async (req, res) => {
-  res.send("ok");
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new CustomError.BadRequestError("Please provide both values");
+  }
+  const user = await User.findOne({ _id: req.user.userId });
+
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+  user.password = newPassword;
+
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: "Success! Password Updated." });
 };
 
 module.exports = {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,8 +16,8 @@ router
   .route("/")
   .get(authenticateUser, authorizePermissions("admin"), getAllUsers);
 router.route("/showMe").get(showCurrentUser);
-router.route("/updateUser").patch(updateUser);
-router.route("/updateUserPassword").patch(updateUserPassword);
+router.route("/updateUser").patch(authenticateUser, updateUser);
+router.route("/updateUserPassword").patch(authenticateUser, updateUserPassword);
 router.route("/:id").get(authenticateUser, getSingleUser);
 
 module.exports = router;
