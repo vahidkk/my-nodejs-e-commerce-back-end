eb7f0ef3f8a7f5c801550871221023bab9952106
vahidkk <kohandel.vahid@gmail.com>
2022-02-24 17:48:14 +0330

    check permission added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,11 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
-
+const {
+  createTokenUser,
+  attachCookiesToResponse,
+  checkPermissions,
+} = require("../utils");
 const getAllUsers = async (req, res) => {
   const users = await User.find({ role: "user" }).select("-password");
   res.status(StatusCodes.OK).json({ users });
@@ -11,8 +15,10 @@ const getSingleUser = async (req, res) => {
   if (!user) {
     throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
   }
+  checkPermissions(req, res, user._id);
   res.status(StatusCodes.OK).json({ user });
 };
+
 const showCurrentUser = async (req, res) => {
   res.status(StatusCodes.OK).json({ user: req.user });
 };
