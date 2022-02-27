a7e5bcb49294ddf3558c8425601b600af8eae4bd
vahidkk <kohandel.vahid@gmail.com>
2022-02-27 20:15:40 +0330

    security packages added

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const morgan = require("morgan");
 const express = require("express");
 const app = express();
 const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
 //connect  db
 const connectDB = require("./db/connect");
 
@@ -17,6 +21,17 @@ const orderRouter = require("./routes/orderRoutes");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(morgan("tiny"));
 const cookieParser = require("cookie-parser");
