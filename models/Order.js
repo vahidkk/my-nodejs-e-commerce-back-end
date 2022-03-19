396d408d522a3f3e578e2d5b13a57fcfd511911a
vahidkk <kohandel.vahid@gmail.com>
2022-03-19 21:22:52 +0330

    Order model bug fixed 1

--- models/Order.js
+++ models/Order.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const SingleOrderItemSchema = mongoose.Schema({
   name: { type: String, required: true },
-  image: { type: String, required: true },
+  image: { type: [String], required: true },
   price: { type: Number, required: true },
   amount: { type: Number, required: true },
+  color: { type: String, required: true },
   product: {
     type: mongoose.Schema.ObjectId,
     ref: "Product",
